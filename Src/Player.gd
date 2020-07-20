3514ec09d5ff798cbfc91a53c6e82077afcc6057
Vanessa Truong <v.truong9710@outlook.com>
2020-07-20 03:27:52 -0400

    Add idle and run player animations.

--- Src/Player.gd
+++ Src/Player.gd
@@ -6,6 +6,8 @@ const FRICTION = 550
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+
 # Called every frame
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -18,8 +20,13 @@ func _physics_process(delta):
 	# multiply by delta -> acceleration/max speed/friction relative to elapsed time, not frame rate
 	# pixels per frame -> pixels per second
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity) 
